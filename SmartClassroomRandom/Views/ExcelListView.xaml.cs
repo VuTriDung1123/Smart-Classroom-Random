3f8d08b91f406758977fa78967343bde973fe194
AkinaAoi <dungvutri25@gmail.com>
2026-03-05 14:40:57 +0700

    UI import excel

---
+++ SmartClassroomRandom/Views/ExcelListView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartClassroomRandom.Views
+{
+    /// <summary>
+    /// Interaction logic for ExcelListView.xaml
+    /// </summary>
+    public partial class ExcelListView : UserControl
+    {
+        public ExcelListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
